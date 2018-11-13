f6960b456d8e8f1f42d6dfc97a13a42983ca9ab8
Nir Parisian <nir.parisian@gmail.com>
2018-11-13 22:58:36 +0200

    Initalize the app

---
+++ src/library/Database.js
@@ -0,0 +1,19 @@
+import Firebase from 'firebase/app';
+import 'firebase/database';
+
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyABZmCyTfuenlYhCA2OLA4J0iNNhFTkoVk",
+  authDomain: "vue-chat-fed93.firebaseapp.com",
+  databaseURL: "https://vue-chat-fed93.firebaseio.com",
+  projectId: "vue-chat-fed93",
+  storageBucket: "",
+  messagingSenderId: "654840825564"
+};
+
+let App = firebase.initializeApp(config);
+
+export let FirebaseApp = App;
+export let FirebaseDb = App.database();
+export let FirebaseAuth = App.auth();
--- src/main.js
+++ src/main.js
@@ -1,15 +1,41 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+// import store from './store'
 
-Vue.config.productionTip = false
+import "firebase/auth";
+import {
+  FirebaseAuth
+} from "@/library/Database";
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+import veeValidate from "vee-validate";
+import "bulma/css/bulma.css";
+
+import FontAwesome from "@fortawesome/fontawesome-free";
+import VueFontAwesoem from "@fortawesome/vue-fontawesome";
+import FontAwesomeSolid from "@fortawesome/free-solid-svg-icons";
+import VeeValidate from "vee-validate";
+
+FontAwesome.Library.add(FontAwesomeSolid);
+Vue.components("fa-icon", VueFontAwesoem);
+Vue.use(VeeValidate);
+
+Vue.config.productionTip = false;
+
+let baseApp;
+
+FirebaseAuth.onAuthStateChanged(() => {
+  if (!baseApp) {
+    baseApp = new Vue({
+      el: "#app",
+      router,
+      store,
+      components: {
+        App
+      },
+      template: "<App/>"
+    });
+  }
 })
