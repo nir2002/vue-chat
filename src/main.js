78aad671c8935a184c886690fe080f6fc8035748
Nir Parisian <nir.parisian@gmail.com>
2018-11-15 23:34:59 +0200

    basic clean up and installation

--- src/main.js
+++ src/main.js
@@ -3,23 +3,28 @@
 import Vue from "vue";
 import App from "./App";
 import router from "./router";
-// import store from './store'
+import store from './store'
 
 import "firebase/auth";
 import {
   FirebaseAuth
 } from "@/library/Database";
 
-import veeValidate from "vee-validate";
 import "bulma/css/bulma.css";
 
-import FontAwesome from "@fortawesome/fontawesome-free";
-import VueFontAwesoem from "@fortawesome/vue-fontawesome";
-import FontAwesomeSolid from "@fortawesome/free-solid-svg-icons";
+import {
+  library
+} from '@fortawesome/fontawesome-svg-core'
+import {
+  faUserSecret
+} from '@fortawesome/free-solid-svg-icons'
+import {
+  FontAwesomeIcon
+} from '@fortawesome/vue-fontawesome'
 import VeeValidate from "vee-validate";
 
-FontAwesome.Library.add(FontAwesomeSolid);
-Vue.components("fa-icon", VueFontAwesoem);
+library.add(faUserSecret)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(VeeValidate);
 
 Vue.config.productionTip = false;
