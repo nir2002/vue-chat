5ad61695dfd2649750589edba447e199082d0151
Nir Parisian <nir.parisian@gmail.com>
2018-11-18 23:41:28 +0200

    added login path to route

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login';
+import {
+  FirebaseAuth
+} from '@/library/Database'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [{
     path: '/',
-    component: HelloWorld
+    component: Login
   }]
 })
+
+
+router.beforeEach((to, _from, next) => {
+  let auth = FirebaseAuth;
+
+  // redirect not authenticated users to login screen
+  if (auth.currentUser === null && to.path !== '/') {
+    next({
+      path: '/'
+    })
+  } else {
+    next()
+  }
+});
+
+export default router;
